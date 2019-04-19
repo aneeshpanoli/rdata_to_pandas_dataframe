9bb93d42eff46ed90c3184aae4d9bf323f612c57
Aneesh Panoli <aneeshpanoli@gmail.com>
2019-04-19 15:22:13 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -50,14 +50,16 @@ class ConvertRdataToDataFrame:
     '''returns table names in the eset'''
     print(tuple(eset_name.slots.keys()))
     
-  def get_pd_dataframe(self, eset_name, table_name, fn_name):
-    '''run get_table_names() for tables names
-      look up the package docs for fn_names
-      eset_name is the return value of load_rdata_as_eset
+  def get_pd_dataframe(self, eset_name, fn_name):
     '''
-    data = eset_name.slots[table_name]
-    r_frame = self.R[fn_name](data) #fn_name is a function from biobase package
+      table names will give you hints on which function to use
+      look up the package docs for fn_names
+    '''
+#     data = eset_name.slots[table_name]
+    r_frame = self.R[fn_name](eset_name) #fn_name is a function from biobase package
     r_to_pd_frame = pandas2ri.ri2py(r_frame)
     return pd.DataFrame(data=r_to_pd_frame)
+    
+  
     
   
